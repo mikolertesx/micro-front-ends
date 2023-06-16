30f1e0fd9f4b1b70b8f10df99921e416027141ae
Miguel Angel <mgangsal@outlook.com>
2023-06-15 19:08:21 -0500

    Updated webpack

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,20 +1,21 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const domain = process.env.PRODUCTION_NAME;
 
 const prodConfig = {
-  mode: 'production',
+  mode: "production",
   output: {
-    filename: '[name].[contenthash].js',
+    filename: "[name].[contenthash].js",
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}`,
       },
       shared: packageJson.dependencies,
     }),
